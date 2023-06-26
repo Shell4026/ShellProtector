f0f0b939b2ea05b1f7e04d827993553e4a649a78
Shell4026 <rkddkwlenfdl@naver.com>
2023-06-27 02:03:49 +0900

    feat: Delete 3byte encrypt function of XTEA
    It's low security

--- Scripts/XTEAEncrypt.cs
+++ Scripts/XTEAEncrypt.cs
@@ -6,7 +6,6 @@ namespace Shell.Protector
 {
     public static class XTEAEncrypt
     {
-        const uint delta3 = 0x11; //magic number but binary start at 1
         const uint delta4 = 0xB9; //magic number but binary start at 1
         const uint delta8 = 0x9E3779B9;
         public static byte[] Encrypt(byte[] data, byte[] key, int rounds = 32)
@@ -22,8 +21,6 @@ namespace Shell.Protector
                     return Encrypt8(data, key, rounds);
                 case 4:
                     return Encrypt4(data, key, rounds);
-                case 3:
-                    return Encrypt3(data, key, rounds);
                 default:
                     return null;
             }
@@ -41,8 +38,6 @@ namespace Shell.Protector
                     return Decrypt8(data, key, rounds);
                 case 4:
                     return Decrypt4(data, key, rounds);
-                case 3:
-                    return Decrypt3(data, key, rounds);
                 default:
                     return null;
             }
@@ -190,102 +185,6 @@ namespace Shell.Protector
                 v0 &= 0x0000FFFF;
             }
             return new byte[4] { (byte)(v0), (byte)(v0 >> 8), (byte)(v1), (byte)(v1 >> 8) };
-        }
-        public static byte[] Encrypt3(byte[] data, byte[] key, int rounds = 32)
-        {
-            if (data.Length != 3)
-            {
-                Debug.LogWarning("The data requires 3 bytes!");
-                return null;
-            }
-            if (key.Length != 16)
-            {
-                Debug.LogWarning("The key requires 16 byte!");
-                return null;
-            }
-
-            //data 3byte, key 16byte
-            uint v0 = (uint)((data[0] | data[1] << 8) & 0xFFF); //12bit
-            uint v1 = (uint)((data[1] >> 4 | data[2] << 4) & 0xFFF); //12bit
-            uint sum = 0; //12bit
-
-            uint[] k = new uint[11];
-
-            k[0] = (uint)((key[0]      | (key[1] << 8)) & 0xFFF); //12bit
-            k[1] = (uint)((key[1] >> 4 | (key[2] << 4)) & 0xFFF); //12bit
-
-            k[2] = (uint)((key[3]      | (key[4] << 8)) & 0xFFF); //12bit
-            k[3] = (uint)((key[4] >> 4 | (key[5] << 4)) & 0xFFF); //12bit
-
-            k[4] = (uint)((key[6]      | (key[7] << 8)) & 0xFFF); //12bit
-            k[5] = (uint)((key[7] >> 4 | (key[8] << 4)) & 0xFFF); //12bit
-
-            k[6] = (uint)((key[9]       | (key[10] << 8)) & 0xFFF); //12bit
-            k[7] = (uint)((key[10] >> 4 | (key[11] << 4)) & 0xFFF); //12bit
-
-            k[8] = (uint)((key[12]      | (key[13] << 8)) & 0xFFF); //12bit
-            k[9] = (uint)((key[13] >> 4 | (key[14] << 4)) & 0xFFF); //12bit
-
-            k[10] = (uint)(key[15]); //8bit
-
-            for (int i = 0; i < rounds; ++i)
-            {
-                v0 += (v1 << 4 ^ v1 >> 5) + v1 ^ sum + k[sum & 10];
-                v0 &= 0x00000FFF;
-                sum += delta3;
-                v1 += (v0 << 4 ^ v0 >> 5) + v0 ^ sum + k[sum >> 2 & 10];
-                v1 &= 0x00000FFF;
-            }
-            
-            
-            return new byte[3] { (byte)(v0 & 0xFF), (byte)((v0 >> 8 | v1 << 4) & 0xFF), (byte)((v1 >> 4) & 0xFF) };
-        }
-        public static byte[] Decrypt3(byte[] data, byte[] key, int rounds = 32)
-        {
-            if (data.Length != 3)
-            {
-                Debug.LogWarning("The data requires 3 bytes!");
-                return null;
-            }
-            if (key.Length != 16)
-            {
-                Debug.LogWarning("The key requires 16 byte!");
-                return null;
-            }
-
-            //data 3byte, key 16byte
-            uint v0 = (uint)((data[0] | data[1] << 8) & 0xFFF); //12bit
-            uint v1 = (uint)((data[1] >> 4 | data[2] << 4) & 0xFFF); //12bit
-            uint sum = (uint)(delta3 * rounds); //12bit
-
-            uint[] k = new uint[11];
-
-            k[0] = (uint)((key[0] | (key[1] << 8)) & 0xFFF); //12bit
-            k[1] = (uint)((key[1] >> 4 | (key[2] << 4)) & 0xFFF); //12bit
-
-            k[2] = (uint)((key[3] | (key[4] << 8)) & 0xFFF); //12bit
-            k[3] = (uint)((key[4] >> 4 | (key[5] << 4)) & 0xFFF); //12bit
-
-            k[4] = (uint)((key[6] | (key[7] << 8)) & 0xFFF); //12bit
-            k[5] = (uint)((key[7] >> 4 | (key[8] << 4)) & 0xFFF); //12bit
-
-            k[6] = (uint)((key[9] | (key[10] << 8)) & 0xFFF); //12bit
-            k[7] = (uint)((key[10] >> 4 | (key[11] << 4)) & 0xFFF); //12bit
-
-            k[8] = (uint)((key[12] | (key[13] << 8)) & 0xFFF); //12bit
-            k[9] = (uint)((key[13] >> 4 | (key[14] << 4)) & 0xFFF); //12bit
-
-            k[10] = (uint)(key[15]); //8bit
-
-            for (int i = 0; i < rounds; ++i)
-            {
-                v1 -= (v0 << 4 ^ v0 >> 5) + v0 ^ sum + k[sum >> 2 & 0b1010];
-                v1 &= 0x00000FFF;
-                sum -= delta3;
-                v0 -= (v1 << 4 ^ v1 >> 5) + v1 ^ sum + k[sum & 0b1010];
-                v0 &= 0x00000FFF;
-            }
-            return new byte[3] { (byte)(v0 & 0xFF), (byte)((v0 >> 8 | v1 << 4) & 0xFF), (byte)((v1 >> 4) & 0xFF) };
         }
     }
 }
