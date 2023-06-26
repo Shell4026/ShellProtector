ad821bee88ed20a8cc3899061ce2a5c802b5e633
Shell4026 <rkddkwlenfdl@naver.com>
2023-06-27 00:47:01 +0900

    feat: Add XXTEA algorithm
    Add only classes for now

---
+++ Scripts/XXTEA.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using UnityEngine;
+
+public class XXTEA
+{
+    const uint Delta = 0x9e3779b9;
+    public static uint[] Encrypt(uint[] data, byte[] key) {
+        uint n = (uint)data.Length;
+
+        uint[] result = new uint[n];
+        Array.Copy(data, result, n);
+        if (n < 2)
+        {
+            Debug.LogError("Data must be minimum 8 bytes!");
+            return null;
+        }
+        uint y, z, sum;
+        uint p, rounds, e;
+        
+        rounds = 6 + 52 / n;
+        sum = 0;
+        z = result[n - 1];
+        do
+        {
+            sum += Delta;
+            e = (sum >> 2) & 3;
+            for (p = 0; p < n - 1; p++)
+            {
+                y = result[p + 1];
+                z = result[p] += (((z >> 5 ^ y << 2) + (y >> 3 ^ z << 4)) ^ ((sum ^ y) + (key[(p & 3) ^ e] ^ z)));
+            }
+            y = result[0];
+            z = result[n - 1] += (((z >> 5 ^ y << 2) + (y >> 3 ^ z << 4)) ^ ((sum ^ y) + (key[(p & 3) ^ e] ^ z)));
+        } while (--rounds > 0);
+        return result;
+    }
+    public static uint[] Decrypt(uint[] data, byte[] key)
+    {
+        uint n = (uint)data.Length;
+
+        uint[] result = new uint[n];
+        Array.Copy(data, result, n);
+
+        if (n < 2)
+        {
+            Debug.LogError("Data must be minimum 8 bytes!");
+            return null;
+        }
+        uint y, z, sum;
+        uint p, rounds, e;
+
+        rounds = 6 + 52 / n;
+        sum = rounds * Delta;
+        y = result[0];
+        do
+        {
+            e = (sum >> 2) & 3;
+            for (p = n-1; p > 0; p--)
+            {
+                z = result[p - 1];
+                y = result[p] -= (((z >> 5 ^ y << 2) + (y >> 3 ^ z << 4)) ^ ((sum ^ y) + (key[(p & 3) ^ e] ^ z)));
+            }
+            z = result[n - 1];
+            y = result[0] -= (((z >> 5 ^ y << 2) + (y >> 3 ^ z << 4)) ^ ((sum ^ y) + (key[(p & 3) ^ e] ^ z)));
+            sum -= Delta;
+        } while (--rounds > 0);
+        return result;
+    }
+}
