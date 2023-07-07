f8cc50730db7d4d560d3e09e7e79488a645b7b7d
Shell4026 <rkddkwlenfdl@naver.com>
2023-07-08 01:45:10 +0900

    feat: Add crunch warning

--- Scripts/EncryptTexture.cs
+++ Scripts/EncryptTexture.cs
@@ -130,14 +130,16 @@ namespace Shell.Protector
             if (texture.format == TextureFormat.DXT1 || texture.format == TextureFormat.DXT1Crunched)
             {
                 Texture2D dxt1 = tex;
-                if(texture.format == TextureFormat.DXT1Crunched)
+                if (texture.format == TextureFormat.DXT1Crunched)
                 {
+                    Debug.LogWarningFormat("{0} is the crunch compression format. There may be degradation in image quality.", tex.name);
                     dxt1 = new Texture2D(tex.width, tex.height, TextureFormat.RGB24, mip_lv, true);
                     for (int m = 0; m <= mip_lv; ++m)
                     {
                         if (m != 0 && m == mip_lv)
                             break;
                         dxt1.SetPixels32(tex.GetPixels32(m), m);
+                        dxt1.Apply();
                     }
                     dxt1.Compress(false);
                 }
@@ -172,7 +174,7 @@ namespace Shell.Protector
                         uint[] data = new uint[2];
                         data[0] = (uint)(tex_data[i + 0] + (tex_data[i + 1] << 8) + (tex_data[i + 2] << 16) + (tex_data[i + 3] << 24));
                         data[1] = (uint)(tex_data[(i + 8) + 0] + (tex_data[(i + 8) + 1] << 8) + (tex_data[(i + 8) + 2] << 16) + (tex_data[(i + 8) + 3] << 24));
-                        
+
                         uint[] data_enc = XXTEA.Encrypt(data, key_uint);
 
                         for (int j = 0; j < 2; ++j)
@@ -199,19 +201,21 @@ namespace Shell.Protector
                 }
                 tmp[0].LoadRawTextureData(raw_data);
             }
-            else if(texture.format == TextureFormat.DXT5 || texture.format == TextureFormat.DXT5Crunched)
+            else if (texture.format == TextureFormat.DXT5 || texture.format == TextureFormat.DXT5Crunched)
             {
                 Texture2D dxt5 = tex;
                 if (texture.format == TextureFormat.DXT5Crunched)
                 {
+                    Debug.LogWarningFormat("{0} is the crunch compression format. There may be degradation in image quality.", tex.name);
                     dxt5 = new Texture2D(tex.width, tex.height, TextureFormat.RGBA32, mip_lv, true);
                     for (int m = 0; m < mip_lv; ++m)
                     {
                         if (m != 0 && m == mip_lv)
                             break;
                         dxt5.SetPixels32(tex.GetPixels32(m), m);
+                        dxt5.Apply();
                     }
-                        
+
                     dxt5.Compress(true);
                 }
                 mip_lv = GetCanMipmapLevel(tex.width, tex.height, true);
@@ -272,74 +276,79 @@ namespace Shell.Protector
                 }
                 tmp[0].LoadRawTextureData(raw_data);
             }
-            else
+            else if (tex.format == TextureFormat.RGBA32)
             {
-                if (HasAlpha(tex))
+                tmp[0] = new Texture2D(tex.width, tex.height, TextureFormat.RGBA32, mip_lv - 2, true); //mip_lv-2 is blur trick (like the box filter)
+                for (int m = 0; m < tmp[0].mipmapCount; ++m)
                 {
-                    tmp[0] = new Texture2D(tex.width, tex.height, TextureFormat.RGBA32, mip_lv - 2, true); //mip_lv-2 is blur trick (like the box filter)
-                    for (int m = 0; m < tmp[0].mipmapCount; ++m)
+                    Color32[] pixels = tex.GetPixels32(m);
+
+                    for (int i = 0; i < pixels.Length; i += 2)
                     {
-                        Color32[] pixels = tex.GetPixels32(m);
+                        key_uint[3] = (uint)i;
 
-                        for (int i = 0; i < pixels.Length; i += 2)
-                        {
-                            key_uint[3] = (uint)i;
+                        uint[] data = new uint[2];
+                        data[0] = (uint)(pixels[i + 0].r + (pixels[i + 0].g << 8) + (pixels[i + 0].b << 16) + (pixels[i + 0].a << 24));
+                        data[1] = (uint)(pixels[i + 1].r + (pixels[i + 1].g << 8) + (pixels[i + 1].b << 16) + (pixels[i + 1].a << 24));
 
-                            uint[] data = new uint[2];
-                            data[0] = (uint)(pixels[i + 0].r + (pixels[i + 0].g << 8) + (pixels[i + 0].b << 16) + (pixels[i + 0].a << 24));
-                            data[1] = (uint)(pixels[i + 1].r + (pixels[i + 1].g << 8) + (pixels[i + 1].b << 16) + (pixels[i + 1].a << 24));
+                        uint[] data_enc = XXTEA.Encrypt(data, key_uint);
 
-                            uint[] data_enc = XXTEA.Encrypt(data, key_uint);
-
-                            pixels[i + 0].r = (byte)((data_enc[0] & 0x000000FF) >> 0);
-                            pixels[i + 0].g = (byte)((data_enc[0] & 0x0000FF00) >> 8);
-                            pixels[i + 0].b = (byte)((data_enc[0] & 0x00FF0000) >> 16);
-                            pixels[i + 0].a = (byte)((data_enc[0] & 0xFF000000) >> 24);
-                            pixels[i + 1].r = (byte)((data_enc[1] & 0x000000FF) >> 0);
-                            pixels[i + 1].g = (byte)((data_enc[1] & 0x0000FF00) >> 8);
-                            pixels[i + 1].b = (byte)((data_enc[1] & 0x00FF0000) >> 16);
-                            pixels[i + 1].a = (byte)((data_enc[1] & 0xFF000000) >> 24);
-                        }
-                        tmp[0].SetPixels32(pixels, m);
+                        pixels[i + 0].r = (byte)((data_enc[0] & 0x000000FF) >> 0);
+                        pixels[i + 0].g = (byte)((data_enc[0] & 0x0000FF00) >> 8);
+                        pixels[i + 0].b = (byte)((data_enc[0] & 0x00FF0000) >> 16);
+                        pixels[i + 0].a = (byte)((data_enc[0] & 0xFF000000) >> 24);
+                        pixels[i + 1].r = (byte)((data_enc[1] & 0x000000FF) >> 0);
+                        pixels[i + 1].g = (byte)((data_enc[1] & 0x0000FF00) >> 8);
+                        pixels[i + 1].b = (byte)((data_enc[1] & 0x00FF0000) >> 16);
+                        pixels[i + 1].a = (byte)((data_enc[1] & 0xFF000000) >> 24);
                     }
-                }
-                else
-                {
-                    tmp[0] = new Texture2D(tex.width, tex.height, TextureFormat.RGB24, mip_lv - 2, true); //mip_lv-2 is blur trick (like the box filter)
-                    for (int m = 0; m < tmp[0].mipmapCount; ++m)
-                    {
-                        Color32[] pixels = tex.GetPixels32(m);
-
-                        for (int i = 0; i < pixels.Length; i += 4)
-                        {
-                            key_uint[3] = (uint)i;
-
-                            uint[] data = new uint[3];
-                            data[0] = (uint)(pixels[i + 0].r + (pixels[i + 0].g << 8) + (pixels[i + 0].b << 16) + (pixels[i + 1].r << 24));
-                            data[1] = (uint)(pixels[i + 1].g + (pixels[i + 1].b << 8) + (pixels[i + 2].r << 16) + (pixels[i + 2].g << 24));
-                            data[2] = (uint)(pixels[i + 2].b + (pixels[i + 3].r << 8) + (pixels[i + 3].g << 16) + (pixels[i + 3].b << 24));
-
-                            uint[] data_enc = XXTEA.Encrypt(data, key_uint);
-
-                            pixels[i + 0].r = (byte)((data_enc[0] & 0x000000FF) >> 0);
-                            pixels[i + 0].g = (byte)((data_enc[0] & 0x0000FF00) >> 8);
-                            pixels[i + 0].b = (byte)((data_enc[0] & 0x00FF0000) >> 16);
-                            pixels[i + 1].r = (byte)((data_enc[0] & 0xFF000000) >> 24);
-                            pixels[i + 1].g = (byte)((data_enc[1] & 0x000000FF) >> 0);
-                            pixels[i + 1].b = (byte)((data_enc[1] & 0x0000FF00) >> 8);
-                            pixels[i + 2].r = (byte)((data_enc[1] & 0x00FF0000) >> 16);
-                            pixels[i + 2].g = (byte)((data_enc[1] & 0xFF000000) >> 24);
-                            pixels[i + 2].b = (byte)((data_enc[2] & 0x000000FF) >> 0);
-                            pixels[i + 3].r = (byte)((data_enc[2] & 0x0000FF00) >> 8);
-                            pixels[i + 3].g = (byte)((data_enc[2] & 0x00FF0000) >> 16);
-                            pixels[i + 3].b = (byte)((data_enc[2] & 0xFF000000) >> 24);
-                        }
-                        tmp[0].SetPixels32(pixels, m);
-                    }
+                    tmp[0].SetPixels32(pixels, m);
                 }
             }
-            tmp[0].filterMode = FilterMode.Point;
-            tmp[0].anisoLevel = 0;
+            else if (tex.format == TextureFormat.RGB24)
+            {
+                tmp[0] = new Texture2D(tex.width, tex.height, TextureFormat.RGB24, mip_lv - 2, true); //mip_lv-2 is blur trick (like the box filter)
+                for (int m = 0; m < tmp[0].mipmapCount; ++m)
+                {
+                    Color32[] pixels = tex.GetPixels32(m);
+
+                    for (int i = 0; i < pixels.Length; i += 4)
+                    {
+                        key_uint[3] = (uint)i;
+
+                        uint[] data = new uint[3];
+                        data[0] = (uint)(pixels[i + 0].r + (pixels[i + 0].g << 8) + (pixels[i + 0].b << 16) + (pixels[i + 1].r << 24));
+                        data[1] = (uint)(pixels[i + 1].g + (pixels[i + 1].b << 8) + (pixels[i + 2].r << 16) + (pixels[i + 2].g << 24));
+                        data[2] = (uint)(pixels[i + 2].b + (pixels[i + 3].r << 8) + (pixels[i + 3].g << 16) + (pixels[i + 3].b << 24));
+
+                        uint[] data_enc = XXTEA.Encrypt(data, key_uint);
+
+                        pixels[i + 0].r = (byte)((data_enc[0] & 0x000000FF) >> 0);
+                        pixels[i + 0].g = (byte)((data_enc[0] & 0x0000FF00) >> 8);
+                        pixels[i + 0].b = (byte)((data_enc[0] & 0x00FF0000) >> 16);
+                        pixels[i + 1].r = (byte)((data_enc[0] & 0xFF000000) >> 24);
+                        pixels[i + 1].g = (byte)((data_enc[1] & 0x000000FF) >> 0);
+                        pixels[i + 1].b = (byte)((data_enc[1] & 0x0000FF00) >> 8);
+                        pixels[i + 2].r = (byte)((data_enc[1] & 0x00FF0000) >> 16);
+                        pixels[i + 2].g = (byte)((data_enc[1] & 0xFF000000) >> 24);
+                        pixels[i + 2].b = (byte)((data_enc[2] & 0x000000FF) >> 0);
+                        pixels[i + 3].r = (byte)((data_enc[2] & 0x0000FF00) >> 8);
+                        pixels[i + 3].g = (byte)((data_enc[2] & 0x00FF0000) >> 16);
+                        pixels[i + 3].b = (byte)((data_enc[2] & 0xFF000000) >> 24);
+                    }
+                    tmp[0].SetPixels32(pixels, m);
+                }
+            }
+            else
+            {
+                Debug.LogErrorFormat("{0} is not supported texture format!", tex.name);
+                tmp[0] = null;
+            }
+            if (tmp[0])
+            {
+                tmp[0].filterMode = FilterMode.Point;
+                tmp[0].anisoLevel = 0;
+            }
             return tmp;
         }
         public Texture2D TextureDecryptXXTEA(Texture2D texture, byte[] key)
