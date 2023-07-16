811cddce57a896b627c0431229b0502647c4e866
Shell4026 <rkddkwlenfdl@naver.com>
2023-07-16 20:09:17 +0900

    fix: Can't build

--- Editor/ShellProtectorEditor.cs
+++ Editor/ShellProtectorEditor.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿#if UNITY_EDITOR
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
@@ -238,3 +239,4 @@ namespace Shell.Protector
         }
     }
 }
+#endif
