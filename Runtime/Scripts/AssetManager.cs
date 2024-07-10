9b703fa7c9dff5d02ef4d718f7db9fa78ac89d4b
Shell <104874910+Shell4026@users.noreply.github.com>
2024-07-11 01:12:31 +0900

    feat: Support poiyomi 9.1, pro

--- Runtime/Scripts/AssetManager.cs
+++ Runtime/Scripts/AssetManager.cs
@@ -29,6 +29,7 @@ namespace Shell.Protector
             support_version.Add("Poiyomi 8.1", 81);
             support_version.Add("Poiyomi 8.2", 82);
             support_version.Add("Poiyomi 9.0", 90);
+            support_version.Add("Poiyomi 9.1", 91);
             support_version.Add("lilToon", 0);
         }
         public bool IsPoiyomi(Shader shader)
@@ -61,7 +62,9 @@ namespace Shell.Protector
             if (poiyomiLabel != -1)
             {
                 var str = shader.GetPropertyDescription(poiyomiLabel);
-                if (str.Contains("Poiyomi 9.0"))
+                if (str.Contains("Poiyomi 9.1"))
+                    return support_version["Poiyomi 9.1"];
+                else if (str.Contains("Poiyomi 9.0"))
                     return support_version["Poiyomi 9.0"];
                 else if(str.Contains("Poiymoi 8.0"))
                     return support_version["Poiyomi 8.0"];
