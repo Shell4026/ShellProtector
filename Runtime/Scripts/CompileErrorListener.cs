a26108e5de5c918c0721f3fc048f026c664373c7
Shell <104874910+Shell4026@users.noreply.github.com>
2025-11-17 01:03:22 +0900

    feat: Check the retry count when the shader changed

--- Runtime/Scripts/CompileErrorListener.cs
+++ Runtime/Scripts/CompileErrorListener.cs
@@ -1,6 +1,4 @@
 ﻿#if UNITY_EDITOR
-using System.Collections;
-using System.Collections.Generic;
 using UnityEditor;
 using UnityEditor.Compilation;
 using UnityEngine;
@@ -10,29 +8,52 @@ namespace Shell.Protector
     [InitializeOnLoad]
     public class CompileErrorListener
     {
+        private static int retryLimit = 3;
+        private static int retryCount = 0;
+
         static CompileErrorListener()
         {
-            CompilationPipeline.assemblyCompilationFinished += (string s, CompilerMessage[] message) =>
-            {
-                foreach (var m in message)
-                {
-                    if (m.type == CompilerMessageType.Error)
-                    {
-                        if
-                        (
-                            m.message.Contains("The type or namespace name 'lilToonInspector' could not be found") ||
-                            m.message.Contains("The type or namespace name 'Thry' could not be found") ||
-                            m.message.Contains("The name 'ShaderOptimizer' does not exist in the current context")
-                        )
-                        {
-                            AssetManager.GetInstance().ResetDefine();
-                        }
-                        else
-                            break;
-                    }
-                }
+            CompilationPipeline.assemblyCompilationFinished += OnAssemblyCompilationFinished;
+        }
 
-            };
+        private static void OnAssemblyCompilationFinished(string assembly, CompilerMessage[] messages)
+        {
+            bool foundTargetError = false;
+
+            foreach (var m in messages)
+            {
+                if (m.type != CompilerMessageType.Error)
+                    continue;
+
+                if (
+                    m.message.Contains("The type or namespace name 'lilToonInspector' could not be found") ||
+                    m.message.Contains("The type or namespace name 'Thry' could not be found") ||
+                    m.message.Contains("The name 'ShaderOptimizer' does not exist in the current context")
+                )
+                {
+                    foundTargetError = true;
+                    break;
+                }
+            }
+
+            if (foundTargetError)
+            {
+                retryCount++;
+
+                if (retryCount <= retryLimit)
+                {
+                    Debug.LogErrorFormat("Shader check error -> ResetDefine {0}/{0}", retryCount, retryLimit);
+                    AssetManager.GetInstance().ResetDefine();
+                }
+                else
+                {
+                    Debug.LogError("Failed ResetDefine");
+                }
+            }
+            else
+            {
+                retryCount = 0;
+            }
         }
     }
 }
